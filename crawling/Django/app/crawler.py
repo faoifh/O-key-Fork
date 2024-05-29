8f16fcbfb9426b31a44e96377c79c9ebed3fd47a
faoifh <104908477+faoifh@users.noreply.github.com>
2024-05-29 21:00:04 +0900

    feat: 중앙일보 크롤링 기능 추가

--- crawling/Django/app/crawler.py
+++ crawling/Django/app/crawler.py
@@ -3,11 +3,12 @@ import json
 from bs4 import BeautifulSoup
 
 def crawling(crawl_type) :
-    #경향신문 O, 국민일보 X, 내일신문 O , 동아일보 O, 문화일보, 서울신문, 세계일보, 아시아투데이, 조선일보, 중앙일보, 한겨레, 한국일보
+    #경향신문 O, 국민일보 X, 내일신문 O , 동아일보 O, 문화일보, 서울신문, 세계일보, 아시아투데이, 조선일보, 중앙일보 O , 한겨레 (크롤링 허용 X), 한국일보 (크롤링 허용 X)
 
     data_bundle = crawl_khan(crawl_type) # 경향신문 크롤링
     data_bundle.extend(crawl_naeil(crawl_type)) #내일신문 크롤링
     data_bundle.extend(crawl_donga(crawl_type)) #동아일보 크롤링
+    data_bundle.extend(crawl_joongang(crawl_type)) #중앙일보 크롤링
 
     #JSON 형식으로 데이터 변환 및 타입 태그 추가
     data = {
@@ -142,6 +143,41 @@ def crawl_donga(crawl_type) : # 동아일보 크롤링
 
     return data_bundle
 
+def crawl_joongang(crawl_type) : # 중앙일보 크롤링
+    TYPETAG = {'politics' : 'politics', 
+               'economy' : 'money', 'society' : 'society', 'culture' : 'culture', 'world' : 'world', 'sport' : 'sports'} # 타입에 따른 주소 태그의 딕셔너리
+
+    if not (crawl_type in TYPETAG) :
+        return {}
+
+    header = {'User-agent' : 'Mozila/2.0'}  
+    data_bundle=[] # 뉴스 정보를 담고 있는 JSON들의 배열
+    
+    res = requests.get("https://www.joongang.co.kr/"+str(TYPETAG[crawl_type]), headers=header)
+    html = res.text
+    soup = BeautifulSoup(html, 'html.parser')
+
+    newsBox = soup.find(class_='story_list')
+    news = newsBox.select('.headline')
+    #print(news)
+    for link in news: # 뉴스마다 제목과 내용을 따옴
+        subRes = requests.get(link.find('a').attrs['href'], headers=header)
+        html = subRes.text
+
+        subSoup = BeautifulSoup(html, 'html.parser')
+        article_text = subSoup.select('#article_body')
+        #print(article_text)
+        title = link.text.strip()
+
+        for letter in article_text: # 내용 따오기
+            content = letter.text.strip().replace('\n', '') # 개행 제거
+
+        # 제목과 내용 배열에 삽입
+        data_bundle.append({"title": title, "content": content})
+
+    #print(data_bundle)
+
+    return data_bundle
 
 
 
