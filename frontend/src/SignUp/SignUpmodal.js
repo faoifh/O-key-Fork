fb25530957154d4a15afb2bfedcb92e086214aca
anhye0n <anhye0ndev@gmail.com>
2024-06-09 17:49:19 +0900

    feat: 회원가입 서버 연결

--- frontend/src/SignUp/SignUpmodal.js
+++ frontend/src/SignUp/SignUpmodal.js
@@ -1,25 +1,51 @@
-import React, { useState } from 'react';
+import React, {useState} from 'react';
 import styles from './SignUpmodal.module.css';
 
-function SignUpmodal({ onClose }) {
+import requestApi from "../plugins/api-setting";
+
+function SignUpmodal({onClose}) {
+
     const [keywords, setKeywords] = useState([
-        { id: 1, text: '정치', checked: false },
-        { id: 2, text: '경제', checked: false },
-        { id: 3, text: '사회', checked: false },
-        { id: 4, text: '문화', checked: false },
-        { id: 5, text: '국제', checked: false },
-        { id: 6, text: '지역', checked: false },
-        { id: 7, text: '스포츠', checked: false },
-        { id: 8, text: 'IT/과학', checked: false },
+        {id: 1, text: '정치', checked: false},
+        {id: 2, text: '경제', checked: false},
+        {id: 3, text: '사회', checked: false},
+        {id: 4, text: '문화', checked: false},
+        {id: 5, text: '국제', checked: false},
+        {id: 6, text: '지역', checked: false},
+        {id: 7, text: '스포츠', checked: false},
+        {id: 8, text: 'IT/과학', checked: false},
     ]);
+
+    const [signUpInfo, setSignUpInfo] = useState({
+        name: "",
+        id: "",
+        password: "",
+    })
 
     const handleCheckboxChange = (id) => {
         setKeywords((prevKeywords) =>
             prevKeywords.map((keyword) =>
-                keyword.id === id ? { ...keyword, checked: !keyword.checked } : keyword
+                keyword.id === id ? {...keyword, checked: !keyword.checked} : keyword
             )
         );
     };
+
+    // 회원가입
+    const signUp = () => {
+        requestApi.post("user/register", {
+            id: signUpInfo.id,
+            name: signUpInfo.name,
+            password: signUpInfo.password,
+            interests: keywords
+                .filter(keyword => keyword.checked)
+                .map(keyword => keyword.text)
+                .toString()
+        }).then(res => {
+            console.log(res)
+        }).catch(err => {
+            console.log(err)
+        })
+    }
 
     return (
         <div className={styles.modal_container}>
@@ -31,15 +57,32 @@ function SignUpmodal({ onClose }) {
                 <div className={styles.modalinputWrap}>
                     <div className={styles.inputname}>
                         <p>NAME</p>
-                        <input className={styles.input_info} placeholder="이름을 입력하세요." />
+                        <input className={styles.input_info}
+                               placeholder="이름을 입력하세요."
+                               value={signUpInfo.name}
+                               onChange={e => setSignUpInfo({
+                                   ...signUpInfo,
+                                   name: e.target.value
+                               })}
+                        />
                     </div>
                     <div className={styles.inputemail}>
                         <p>E-mail</p>
-                        <input className={styles.input_info} placeholder="사용할 이메일을 입력하세요." />
+                        <input className={styles.input_info} placeholder="사용할 이메일을 입력하세요."
+                               value={signUpInfo.id}
+                               onChange={e => setSignUpInfo({
+                                   ...signUpInfo,
+                                   id: e.target.value
+                               })}/>
                     </div>
                     <div className={styles.inputpw}>
                         <p>Password</p>
-                        <input className={styles.input_info} placeholder="사용할 비밀번호를 입력하세요." />
+                        <input className={styles.input_info} placeholder="사용할 비밀번호를 입력하세요."
+                               value={signUpInfo.password}
+                               onChange={e => setSignUpInfo({
+                                   ...signUpInfo,
+                                   password: e.target.value
+                               })}/>
                     </div>
                 </div>
                 <div className={styles.checkboxWrap}>
@@ -60,7 +103,7 @@ function SignUpmodal({ onClose }) {
                     </div>
                 </div>
                 <div className={styles.signup}>
-                    <button className={styles.signupbtn}>회원가입</button>
+                    <button className={styles.signupbtn} onClick={signUp}>회원가입</button>
                 </div>
             </div>
         </div>
