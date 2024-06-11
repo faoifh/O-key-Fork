12762e870a6b5514d8d1e50dcae52df2534cff42
anhye0n <anhye0ndev@gmail.com>
2024-06-11 15:58:59 +0900

    feat: 관심 목록 추가

--- frontend/src/Interest_news/Interest_news.js
+++ frontend/src/Interest_news/Interest_news.js
@@ -1,31 +1,32 @@
-import React, {useEffect, useState} from 'react';
+import React, { useEffect, useState } from 'react';
 import styles from './Interest_news.module.css';
 import Menubar from "../components/menubar";
-import {useSelector} from "react-redux";
-import {useNavigate} from "react-router-dom";
-import {keywordsApi} from "../plugins/api-setting";
-
+import { useDispatch, useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import { keywordsApi } from "../plugins/api-setting";
+import axios from "axios";
+import { setInterests } from "../store/user-slice";
 
 export default function Interest_news() {
-
     const [isLoginModalOpen, setIsLoginModalOpen] = useState(false);
     const [isSignUpModalOpen, setIsSignUpModalOpen] = useState(false);
 
     const [keywords, setKeywords] = useState([
-        {id: 1, text: '정치', checked: false},
-        {id: 2, text: '경제', checked: false},
-        {id: 3, text: '사회', checked: false},
-        {id: 4, text: '문화', checked: false},
-        {id: 5, text: '국제', checked: false},
-        {id: 7, text: '스포츠', checked: false},
-        {id: 8, text: 'IT/과학', checked: false},
-        {id: 9, text: '교육', checked: false},
-        {id: 10, text: '엔터테인먼트', checked: false},
-        {id: 11, text: '인물', checked: false},
+        { id: 1, text: '정치', checked: false },
+        { id: 2, text: '경제', checked: false },
+        { id: 3, text: '사회', checked: false },
+        { id: 4, text: '문화', checked: false },
+        { id: 5, text: '국제', checked: false },
+        { id: 7, text: '스포츠', checked: false },
+        { id: 8, text: 'IT/과학', checked: false },
+        { id: 9, text: '교육', checked: false },
+        { id: 10, text: '엔터테인먼트', checked: false },
+        { id: 11, text: '인물', checked: false },
     ]);
-    const reduxInfo = useSelector((state) => state.userInfo)
+    const reduxInfo = useSelector((state) => state.userInfo);
 
-    const navigate = useNavigate()
+    const navigate = useNavigate();
+    const dispatch = useDispatch();
 
     useEffect(() => {
         if (!reduxInfo.userName) {
@@ -34,56 +35,74 @@ export default function Interest_news() {
         }
 
         const updatedKeywords = keywords.map(keyword =>
-            reduxInfo.interests.includes(keyword.text) ? {...keyword, checked: true} : keyword
+            reduxInfo.interests.includes(keyword.text) ? { ...keyword, checked: true } : keyword
         );
 
         setKeywords(updatedKeywords);
     }, [reduxInfo, navigate]);
 
-    const handleCheckboxChange = (id) => {
-        setKeywords((prevKeywords) =>
-            prevKeywords.map((keyword) =>
-                keyword.id === id ? {...keyword, checked: !keyword.checked} : keyword
-            )
+    const handleCheckboxChange = async (id) => {
+        const updatedKeywords = keywords.map((keyword) =>
+            keyword.id === id ? { ...keyword, checked: !keyword.checked } : keyword
         );
+        setKeywords(updatedKeywords);
+
+        const newInterests = updatedKeywords
+            .filter((keyword) => keyword.checked)
+            .map((keyword) => keyword.text);
+        dispatch(setInterests(newInterests));
     };
 
-    function Article({interest}) {
-
+    function Article({ interest }) {
         const [news, setNews] = useState([]);
         const [loading, setLoading] = useState(false);
 
         const newsTypesConvert = [
-            {text: '정치', en: 'politics'},
-            {text: '경제', en: 'economy'},
-            {text: '사회', en: 'society'},
-            {text: '문화', en: 'culture'},
-            {text: '국제', en: 'world'},
-            {text: '스포츠', en: 'sport'},
-            {text: 'IT/과학', en: 'science'},
-            {text: '교육', en: 'education'},
-            {text: '엔터테인먼트', en: 'enter'},
-            {text: '인물', en: 'people'}
+            { text: '정치', en: 'politics' },
+            { text: '경제', en: 'economy' },
+            { text: '사회', en: 'society' },
+            { text: '문화', en: 'culture' },
+            { text: '국제', en: 'world' },
+            { text: '스포츠', en: 'sport' },
+            { text: 'IT/과학', en: 'science' },
+            { text: '교육', en: 'education' },
+            { text: '엔터테인먼트', en: 'enter' },
+            { text: '인물', en: 'people' },
         ];
         const matchingType = newsTypesConvert.find(data => data.text === interest);
 
         useEffect(() => {
             if (matchingType) {
                 setLoading(true);
-                keywordsApi.get(`/news/`, {
-                    params: {
-                        crawl_type: matchingType.en
-                    },
-                    withCredentials: true
-                }).then(res => {
-                    setNews(res.data.data);
-                    setLoading(false);
-                }).catch(err => {
-                    console.log(err);
-                    setLoading(false);
-                });
+                if (process.env.REACT_APP_CRAWLING_API_URL === "http://localhost:8000") {
+                    keywordsApi.get(`/news/`, {
+                        params: {
+                            crawl_type: matchingType.en
+                        },
+                        withCredentials: true
+                    }).then(res => {
+                        setNews(res.data.data || []);
+                        setLoading(false);
+                    }).catch(err => {
+                        console.log(err);
+                        setLoading(false);
+                    });
+                } else {
+                    axios.get(`https://keywords.anhye0n.com/news`, {
+                        params: {
+                            crawl_type: matchingType.en
+                        },
+                        withCredentials: true
+                    }).then(res => {
+                        setNews(res.data.data || []);
+                        setLoading(false);
+                    }).catch(err => {
+                        console.log(err);
+                        setLoading(false);
+                    });
+                }
             }
-        }, []);
+        }, [matchingType]);
 
         return (
             <div>
@@ -98,7 +117,7 @@ export default function Interest_news() {
                             }}>
                                 <h1>{data.title}</h1>
                                 <p className={styles['article-company']}>{data.company}</p>
-                                <hr/>
+                                <hr />
                                 <p>{data.content.length > 300 ? `${data.content.substring(0, 300)}...` : data.content}</p>
                             </div>
                         ))
@@ -110,7 +129,7 @@ export default function Interest_news() {
 
     return (
         <div className={`${styles.app}`}>
-            <Menubar setIsSignUpModalOpen={setIsSignUpModalOpen} setIsLoginModalOpen={setIsLoginModalOpen}/>
+            <Menubar setIsSignUpModalOpen={setIsSignUpModalOpen} setIsLoginModalOpen={setIsLoginModalOpen} />
             <div className={`${styles.content}`}>
                 <div className={`${styles.contentwrapper}`}>
                     <div className={`${styles.keyword_header}`}>
@@ -135,7 +154,7 @@ export default function Interest_news() {
                     </div>
                 </div>
                 <div className={styles['contents-container']}>
-                    {reduxInfo.interests.map(data => <Article interest={data} key={data}/>)}
+                    {reduxInfo.interests.map(data => <Article interest={data} key={data} />)}
                 </div>
             </div>
         </div>
