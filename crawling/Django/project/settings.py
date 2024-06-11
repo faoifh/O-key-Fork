32c0d3f633cdf7e156a39f6dd61f1910d0760398
faoifh <104908477+faoifh@users.noreply.github.com>
2024-06-12 05:34:59 +0900

    feat : 허용된 localhost 주소 추가

--- crawling/Django/project/settings.py
+++ crawling/Django/project/settings.py
@@ -52,7 +52,9 @@ MIDDLEWARE = [
 
 CORS_ALLOWED_ORIGINS = [
     'http://127.0.0.1:3000',
+    'http://127.0.0.1:8000',
     'http://localhost:3000',
+    'http://localhost:8000',
     'https://o-key.anhye0n.com',
 ]
 
